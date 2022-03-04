ad3008ec61aeeb069464a14b51e4d93d11154af8
Thomas Bono <thomasbono.com@gmail.com>
2022-03-04 11:35:58 -0500

    ds lib updated to latest sha

--- plugins/primevue.js
+++ plugins/primevue.js
@@ -1,7 +1,7 @@
 import { defineNuxtPlugin } from "#app"
 import PrimeVue from "primevue/config"
 import Button from "primevue/button"
-import Tag from "primevue/tag"
+// import Tag from "primevue/tag"
 // import Menubar from 'primevue/menubar';
 // import InputText from 'primevue/inputText';
 // import Breadcrumb from 'primevue/breadcrumb';
@@ -10,7 +10,7 @@ import Tag from "primevue/tag"
 export default defineNuxtPlugin((nuxtApp) => {
     nuxtApp.vueApp.use(PrimeVue, { ripple: true })
     nuxtApp.vueApp.component('Button', Button)
-    nuxtApp.vueApp.component('Tag', Tag)
+    // nuxtApp.vueApp.component('Tag', Tag)
     // nuxtApp.vueApp.component('Menubar', Menubar)
     // nuxtApp.vueApp.component('InputText', InputText)
     // nuxtApp.vueApp.component('Breadcrumb', Breadcrumb)
