77fca93ca3e605841e712e038afc7f8b74cfd7b6
Kim LaRocca <kim@kimlarocca.com>
2022-03-09 15:43:02 -0500

    setup unit testing with jest and axe

---
+++ test/RadiolabHeader.spec.js
@@ -0,0 +1,14 @@
+import { mount } from '@vue/test-utils'
+import { toHaveNoViolations } from 'jest-axe'
+import RadiolabHeader from '../components/RadiolabHeader.vue'
+import axe from './axe-helper'
+
+expect.extend(toHaveNoViolations)
+
+describe('Radiolab Header', () => {
+  test('it passes basic accessibility tests', async () => {
+    const wrapper = mount(RadiolabHeader)
+    const results = await axe(wrapper.element)
+    expect(results).toHaveNoViolations()
+  })
+})
