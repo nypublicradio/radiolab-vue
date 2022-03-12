e392f9015cf281151b801c1b2baa7016a59f725d
Thomas Bono <thomasbono.com@gmail.com>
2022-03-12 10:06:37 -0500

    global scss set up

--- layouts/default.vue
+++ layouts/default.vue
@@ -34,39 +34,4 @@ const props = defineProps({
 </template>
 
 <style lang="scss">
-body {
-  background: #ffffff;
-}
-
-:root,
-[data-color-mode="blue"] {
-  --link-button-color: var(--primary-light-color);
-  --text-color: var(--white100);
-  --text-color-secondary: var(--white300);
-}
-div,
-span {
-  &[data-color-mode="blue"] {
-    .p-button:not(.p-button-text) {
-      background: var(--white);
-      color: #0454d6;
-      border-radius: 25px;
-      border-color: #ffffff;
-      span {
-        font-weight: 600;
-      }
-    }
-    .p-inputtext {
-      border-radius: 25px;
-      background-color: rgba(10, 11, 12, 0.1);
-      border: none;
-      padding-left: spacing(6);
-      &::placeholder {
-        /* Chrome, Firefox, Opera, Safari 10.1+ */
-        color: #f4f2f0;
-        opacity: 0.4; /* Firefox */
-      }
-    }
-  }
-}
 </style>
