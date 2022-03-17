7e375de0a00d42e8ce697fdd33d1111d3a8e0c94
Thomas Bono <thomasbono.com@gmail.com>
2022-03-17 13:40:49 -0400

    favicon and meta stuff

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,20 @@ import { defineNuxtConfig } from 'nuxt3'
 
 // https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
+  meta: {
+    meta:[
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: 'msapplication-TileColor', content: '#f4f2f0' },
+      { name: 'theme-color', content: '#f4f2f0' }
+    ],
+    link: [
+      { rel: 'icon', type:'image/png', sizes:'32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type:'image/png', sizes:'16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes:"180x180", href: '/apple-touch-icon.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color:'#ff3904' }
+    ]
+  },
   //buildModules: ['nypr-design-system-vue3/nuxt'],
   css: [
     //'primevue/resources/themes/saga-blue/theme.css',
