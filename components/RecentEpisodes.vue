fa005fd27cd43dc708fdbc3f20bebedcdb77f8c4
Allie Drake <adrake@nypublicradio.org>
2022-03-18 12:51:26 -0700

    cleanup

--- components/RecentEpisodes.vue
+++ components/RecentEpisodes.vue
@@ -1,12 +1,6 @@
 <script setup>
 import { onBeforeMount, ref } from 'vue'
-import { formatDate } from '~/utilities/helpers'
 import axios from 'axios'
-import VCard from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue'
-import VFlexibleLink from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VFlexibleLink.vue'
-import VImageWithCaption from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VImageWithCaption.vue'
-import ApplePodcasts from '~/components/icons/ApplePodcasts.vue'
-import Skeleton from 'primevue/skeleton'
 const dataLoaded = ref(false)
 const episodes = ref([])
 onBeforeMount(async () => {
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,30 +1,22 @@
 import { defineNuxtConfig } from 'nuxt3'
 
-// https://v3.nuxtjs.org/docs/directory-structure/nuxt.config
 export default defineNuxtConfig({
   meta: {
-    meta:[
+    meta: [
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: 'msapplication-TileColor', content: '#f4f2f0' },
       { name: 'theme-color', content: '#f4f2f0' }
     ],
     link: [
-      { rel: 'icon', type:'image/png', sizes:'32x32', href: '/favicon-32x32.png' },
-      { rel: 'icon', type:'image/png', sizes:'16x16', href: '/favicon-16x16.png' },
-      { rel: 'apple-touch-icon', sizes:"180x180", href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'apple-touch-icon', sizes: "180x180", href: '/apple-touch-icon.png' },
       { rel: 'manifest', href: '/site.webmanifest' },
-      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color:'#ff3904' }
+      { rel: 'mask-icon', href: '/safari-pinned-tab.svg', color: '#ff3904' }
     ],
-    // bodyAttrs: {
-    //   ref: 'el',
-    //   class:'bodyy'
-    // }
   },
-  // buildModules: ['nypr-design-system-vue3/nuxt'],
   buildModules: ['@vueuse/nuxt'],
   css: [
-    //'primevue/resources/themes/saga-blue/theme.css',
-    // 'nypr-design-system-vue3/src/assets/library/primeflex.min.css',
     '@nypublicradio/nypr-design-system-vue3/src/assets/themes/radiolab/radiolab.min.css',
     'primevue/resources/primevue.min.css',
     'primeicons/primeicons.css',
