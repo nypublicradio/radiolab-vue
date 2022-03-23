59ccdb3540224cdf6bde478f317173357d437f7d
Thomas Bono <thomasbono.com@gmail.com>
2022-03-23 19:29:37 -0400

    main episode responsive adjustment

--- components/RecentEpisodes.vue
+++ components/RecentEpisodes.vue
@@ -28,24 +28,24 @@ onBeforeMount(async () => {
         <div class="grid">
           <div class="col">
             <div v-if="dataLoaded" class="latest-episode grid grid-nogutter">
-              <div class="col-12 xl:col-7 p-0">
+              <div class="col-12 md:col-7 p-0">
                 <v-image-with-caption
                   :image="episodes[0].attributes['image-main'].template.replace('%s/%s/%s/%s', '%width%/%height%/c/%quality%')"
                   :imageUrl="`/episodes/${episodes[0].attributes.slug}`"
                   :alt="episodes[0].attributes['image-main']['alt-text']"
                   :max-width="episodes[0].attributes['image-main'].w"
                   :max-height="episodes[0].attributes['image-main'].h"
-                  :ratio="[8, 5]"
+                  :ratio="[8, 6]"
                   class="latest-episode-image"
                 />
               </div>
-              <div class="latest-episode-content col-12 xl:col-5 p-4 xl:p-7">
-                <h5 class="mb-2">Latest Episode</h5>
+              <div class="latest-episode-content col-12 md:col-5 p-4 lg:p-7">
+                <h5 class="mb-0 lg:mb-2">Latest Episode</h5>
                 <v-flexible-link
                   :to="`/episodes/${episodes[0].attributes.slug}`"
                   class="latest-episode-title"
                 >
-                  <h2 class="mb-3 inline-block" v-html="episodes[0].attributes.title"></h2>
+                  <h2 class="mb-2 lg:mb-3 inline-block" v-html="episodes[0].attributes.title"></h2>
                 </v-flexible-link>
                 <p
                   v-html="episodes[0].attributes.tease"
@@ -115,6 +115,12 @@ onBeforeMount(async () => {
 
 .latest-episode .latest-episode-title {
   text-decoration: none;
+  @include media("<lg") {
+    h2{
+    font-size: var(--font-size-12);
+    line-height: var(--line-height-12);
+    }
+  }
 }
 
 .latest-episode .latest-episode-image {
