0f33311945660b60edf1bad9768045c3620a8038
bono <13803343+bonomite@users.noreply.github.com>
2022-04-18 16:52:09 -0400

    Bono/rl 33b (#38)
    * visual updates to the 400 page
    
    * visual updates to the 500 page

--- pages/500.vue
+++ pages/500.vue
@@ -3,7 +3,7 @@ useHead({
   meta: [
     {
       name: 'theme-color',
-      content: '#90d8fc',
+      content: colors.rlLightorange,
     },
   ],
   bodyAttrs: {
@@ -15,38 +15,35 @@ useHead({
 <template>
   <div class="error-page">
     <Html>
-      <Head>
-        <Title>500 Error | Radiolab | WNYC Studios</Title>
-        <Meta name="og:title" content="500 Error | Radiolab | WNYC Studios" />
-        <Meta name="twitter:title" content="500 Error | Radiolab | WNYC Studios" />
-      </Head>
+
+    <Head>
+      <Title>500 Error | Radiolab | WNYC Studios</Title>
+      <Meta name="og:title" content="500 Error | Radiolab | WNYC Studios" />
+      <Meta name="twitter:title" content="500 Error | Radiolab | WNYC Studios" />
+    </Head>
+
     </Html>
-    <section class="graphic-head flex justify-content-center align-content-center">
+    <section class="graphic-head lightorange flex justify-content-center align-content-center">
       <h2 class="text-center font-semibold">Nope.</h2>
     </section>
-    <section class="white100">
-      <div class="content p-3 md:p-8">
-        <div class="grid justify-content-between">
-          <div class="col-12 lg:col-5">
+    <section class="white100bg">
+      <div class="content md:p-8 thin-content-width">
+        <div class="grid">
+          <div class="col-12 md:p-6">
             <p class="mb-4">Not all those who wander are lost. But you sure are!</p>
             <p>
-              <strong>500 Error</strong> - something has gone wrong on the web site's server, but you are invited to listen to our
+              <strong>500 Error</strong> - something has gone wrong on the web site's server, but you are invited to
+              listen to our
               <strong>All-Time Top Ten Favorite Radiolab Episodes</strong> below.
             </p>
           </div>
         </div>
       </div>
-      <div class="flex w-full justify-content-end">
-        <img class="error-image" alt="error image" src="/error-404.png" />
-      </div>
     </section>
     <!-- TODO: need top ten endpoint -->
-    <episodes
-      class="mt-6 mb-4"
-      :row-count="3"
+    <episodes class="mt-6 mb-4" :row-count="3"
       api="https://private-anon-26d14f4b2b-nyprpublisher.apiary-proxy.com/api/v3/channel/shows/radiolab/recent_stories/"
-      path="data.included"
-    />
+      path="data.included" />
   </div>
 </template>
 
@@ -54,20 +51,6 @@ useHead({
 .error-page {
   .graphic-head {
     // defaults used for 404 page
-  }
-  .white100 {
-    background-color: var(--white100);
-  }
-  .error-image {
-    position: relative;
-    width: 100%;
-    max-width: 316px;
-    height: auto;
-    margin-top: 0;
-    @include media(">lg") {
-      margin-top: -490px;
-      max-width: 616px;
-    }
   }
 }
 </style>
