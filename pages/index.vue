5852768b99c84d245acbcb41e209fd3690ffcff4
Allie Drake <adrake@nypublicradio.org>
2022-04-19 10:35:09 -0700

    missed one

--- pages/index.vue
+++ pages/index.vue
@@ -39,7 +39,7 @@ const apiUrl = `${config.API_URL}/api/v3/channel/shows/radiolab/recent_stories/`
       button-link="/episodes"
       :row-count="1"
       :startCount="1"
-      api="https://private-anon-26d14f4b2b-nyprpublisher.apiary-proxy.com/api/v3/channel/shows/radiolab/recent_stories/"
+      :api="apiUrl"
       path="data.included"
     />
   </div>
