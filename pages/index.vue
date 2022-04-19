eb9e5c94b157178e4d6bac26bd0751d4fe6990e2
Kim LaRocca <kim@kimlarocca.com>
2022-04-19 13:25:20 -0400

    missing Popular Episodes API url

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <script setup>
 import { useRuntimeConfig } from '#app';
-
 const config = useRuntimeConfig();
-const apiUrl = `${ config.API_URL }/api/v3/channel/shows/radiolab/recent_stories/`
+const recentEpisodesApiUrl = `${ config.API_URL }/api/v3/channel/shows/radiolab/recent_stories/`
+const popularEpisodesApiUrl = `${ config.API_URL }/api/v3/channel/shows/radiolab/recent_stories/`
 </script>
 
 <template>
   <div>
     <latest-episode class="mb-4" />
     <episodes class="mb-4" header="Recent Episodes" button-text="All Episodes" button-link="/episodes" :row-count=" 1 "
-      :startCount=" 1 " :api=" apiUrl " path="data.included" />
+      :startCount=" 1 " :api=" recentEpisodesApiUrl " path="data.included" />
     <div class="htlad-radiolab_in-content_1 mb-8" />
     <exclusive-content-from-the-lab class="mb-8" />
     <section class="mb-8">
@@ -26,7 +26,7 @@ const apiUrl = `${ config.API_URL }/api/v3/channel/shows/radiolab/recent_stories
     </section>
     <radiolab-series class="mb-8" />
     <episodes class="mb-8" header="Popular Episodes" button-text="All Episodes" button-link="/episodes" :row-count=" 1 "
-      :startCount=" 1 " :api=" apiUrl " path="data.included" />
+      :startCount=" 1 " :api=" popularEpisodesApiUrl " path="data.included" />
     <div class="htlad-radiolab_in-content_2 mb-8" />
   </div>
 </template>
