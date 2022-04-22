91b725edefb09cf313f7b7035821caa173be446c
Thomas Bono <thomasbono.com@gmail.com>
2022-04-22 18:03:00 -0400

    style adjustments to error and mobile hamburger

--- pages/500.vue
+++ pages/500.vue
@@ -23,24 +23,17 @@ const apiUrl = `${apiUrl}/api/v3/channel/shows/radiolab/recent_stories/`
       <Head>
         <Title>500 Error | Radiolab | WNYC Studios</Title>
         <Meta name="og:title" content="500 Error | Radiolab | WNYC Studios" />
-        <Meta
-          name="twitter:title"
-          content="500 Error | Radiolab | WNYC Studios"
-        />
+        <Meta name="twitter:title" content="500 Error | Radiolab | WNYC Studios" />
       </Head>
     </Html>
-    <section
-      class="graphic-head lightorange flex justify-content-center align-content-center"
-    >
+    <section class="graphic-head lightorange flex justify-content-center align-content-center">
       <h2 class="text-center font-semibold">Nope.</h2>
     </section>
     <section class="white100bg">
-      <div class="content md:p-8 thin-content-width">
+      <div class="content thin-content-width">
         <div class="grid">
-          <div class="col-12 md:p-6">
-            <p class="mb-4">
-              Not all those who wander are lost. But you sure are!
-            </p>
+          <div class="col-12">
+            <p class="mb-4">Not all those who wander are lost. But you sure are!</p>
             <p>
               <strong>500 Error</strong> - something has gone wrong on the web
               site's server, but you are invited to listen to our
@@ -52,19 +45,21 @@ const apiUrl = `${apiUrl}/api/v3/channel/shows/radiolab/recent_stories/`
       </div>
     </section>
     <!-- TODO: need top ten endpoint -->
-    <episodes
-      class="mt-6 mb-4"
-      :row-count="3"
-      :api="apiUrl"
-      path="data.included"
-    />
+    <episodes class="mt-6 mb-4" :row-count="3" :api="apiUrl" path="data.included" />
   </div>
 </template>
 
 <style lang="scss">
 .error-page {
-  .graphic-head {
-    // defaults used for 404 page
+  .content {
+    padding: 7rem;
+    @include media("<md") {
+      padding: 1rem;
+    }
+    text-align: center;
+    p {
+      font-size: var(--font-size-8);
+    }
   }
 }
 </style>
