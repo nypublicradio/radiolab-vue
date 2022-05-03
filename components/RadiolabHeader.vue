0c134eaacb9436e7556c2b6f16c6aad3237232d8
Kim LaRocca <kim@kimlarocca.com>
2022-05-03 10:52:46 -0400

    update radiolab logo component name

--- components/RadiolabHeader.vue
+++ components/RadiolabHeader.vue
@@ -33,7 +33,7 @@ onMounted(() => {
         <Menubar :model="menuItems" id="p-menu">
           <template #start>
             <nuxt-link to="/" class="logo-holder-link">
-              <radiolab-logo class="logo mr-2" />
+              <icons-radiolab-logo class="logo mr-2" />
             </nuxt-link>
           </template>
           <!--  <template #end>
