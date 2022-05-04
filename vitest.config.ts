44e517a9def69cf1915958349ad333bf55070dac
Thomas Bono <thomasbono.com@gmail.com>
2022-05-04 15:55:23 -0400

    deepsource fix

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig/* , loadEnv */ } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-export default ({ mode }) => {
+export default (/* { mode } */) => {
   // process.env = {...process.env, ...loadEnv(mode, process.cwd())};
   // const envTheme = process.env.VITE_VUE_APP_THEME
 
