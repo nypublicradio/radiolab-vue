235036ad0f476b75050c97f31d172887610a4ad4
Kim LaRocca <kim@kimlarocca.com>
2022-05-05 13:20:32 -0400

    added basic GA4 tracking and a function to track custom events

---
+++ utilities/ga.js
@@ -0,0 +1,13 @@
+// fires a custom google analytics GA4 event
+// note: we use gaAction to specify the component name
+import { useGtag } from 'vue-gtag-next'
+const { event } = useGtag()
+function gaEvent(gaCategory, gaAction, gaLabel) {
+  event('Event', {
+    hit_time_stamp: new Date().toISOString(),
+    component: gaAction,
+    event_category: gaCategory,
+    event_label: gaLabel
+  })
+}
+export default gaEvent
