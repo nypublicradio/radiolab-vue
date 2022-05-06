dcdb87a5c975932fbb4aed0fca76f96515511d49
Kim LaRocca <kim@kimlarocca.com>
2022-05-06 15:15:01 -0400

    new og default image from Kelly

--- layouts/default.vue
+++ layouts/default.vue
@@ -51,9 +51,10 @@ onMounted(() => {
           content="Radiolab: Podcasts | WNYC Studios | Podcasts"
         />
         <Meta name="og:description" content="Investigating a strange world." />
+        <Meta name="og:image" content="/og-default-image.png" />
         <Meta
-          name="og:image"
-          content="https://media.wnyc.org/i/1200/600/l/80/2020/09/Radiolab_OG_Image_Options_3.png"
+          name="og:image:alt"
+          content="Radiolab: Podcasts | WNYC Studios | Podcasts"
         />
         <Meta name="og:image:width" content="1200" />
         <Meta name="og:image:height" content="600" />
@@ -73,10 +74,7 @@ onMounted(() => {
           name="twitter:description"
           content="Investigating a strange world."
         />
-        <Meta
-          name="twitter:image"
-          content="https://media.wnyc.org/i/1200/600/l/80/2020/09/Radiolab_OG_Image_Options_3.png"
-        />
+        <Meta name="twitter:image" content="/images/default-og-image.png" />
         <Meta
           name="apple-itunes-app"
           content="app-id=152249110, affiliate-data=at=1010lSme&amp;ct=radiolab"
