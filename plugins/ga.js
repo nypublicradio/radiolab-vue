874e792480ccefed955c687a33dd62d0c21e1623
Kim LaRocca <kim@kimlarocca.com>
2022-05-06 15:40:09 -0400

    deepsource issues

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,5 +1,7 @@
+import { defineNuxtPlugin } from '#app'
 import VueGtag from 'vue-gtag-next'
 
+// setup vue gtag as a nuxt plugin
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(VueGtag, {
     property: {
