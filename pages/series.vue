f50f91da5e9e0e9b920a89ef01422a1c37565339
Kim LaRocca <kim@kimlarocca.com>
2022-05-12 16:26:35 -0400

    new flat page for series

--- pages/series.vue
+++ pages/series.vue
@@ -1,37 +1,91 @@
 <script setup>
 import { useRuntimeConfig } from '#app'
-
+import { onBeforeMount, ref } from 'vue'
+import axios from 'axios'
+useMeta({
+  bodyAttrs: {
+    class: 'has-head-color',
+  },
+})
+const dataLoaded = ref(false)
+const page = ref([])
+const router = useRouter()
 const config = useRuntimeConfig()
-const apiUrl = `${config.API_URL}/api/v3/channel/shows/radiolab/radio-shows/`
-// const apiUrl = `https://private-anon-c9c388aa36-nyprpublisher.apiary-proxy.com/api/v3/channel/shows/radiolab/radio-shows/`
+
+onBeforeMount(async () => {
+  await axios
+    .get(`${config.API_URL}/api/v3/flatpages/1259/`)
+    .then((response) => {
+      page.value = response.data.data.attributes
+      dataLoaded.value = true
+    })
+    .catch(() => {
+      router.push('/404')
+    })
+})
 </script>
 
 <template>
   <div>
-    <Html>
-      <Head>
-        <Title>Series | Radiolab | WNYC Studios</Title>
-        <Meta name="og:title" content="Series | Radiolab | WNYC Studios" />
-        <Meta name="twitter:title" content="Series | Radiolab | WNYC Studios" />
-      </Head>
-    </Html>
-    <section>
-      <div class="content lg:px-8 pb-0">
-        <div class="grid">
-          <div class="col">
-            <h1 class="h2">Series</h1>
-          </div>
-        </div>
+    <section class="head-color yellow">
+      <div class="content thin-content-width">
+        <template v-if="dataLoaded">
+          <Html>
+            <Head>
+              <Title>Series | Radiolab | WNYC Studios</Title>
+              <Meta
+                name="og:title"
+                content="Series | Radiolab | WNYC Studios"
+              />
+              <Meta
+                name="twitter:title"
+                content="Series | Radiolab | WNYC Studios"
+              />
+            </Head>
+          </Html>
+          <div
+            class="html-formatting"
+            v-html="page['superchunk-head-plus-content']"
+          />
+        </template>
+        <skeleton-general-content v-else />
       </div>
     </section>
-    <episodes
-      class="mb-4"
-      :row-count="4"
-      :api="apiUrl"
-      path="data.included"
-      :paginate="true"
-    />
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.series .projects-landing-page__project {
+  display: grid;
+  grid-template-rows: auto auto;
+  grid-template-columns: auto 1fr;
+  grid-column-gap: 20px;
+  grid-template-areas:
+    'logo title'
+    'logo description';
+  margin-bottom: 46px;
+}
+.projects-landing-page__image {
+  width: 244px;
+  height: 244px;
+  grid-area: logo;
+}
+.projects-landing-page__project a {
+  font-family: var(--font-family-header);
+  font-size: var(--font-size-10);
+  font-weight: var(--font-weight-500);
+  line-height: 28px;
+  grid-area: title;
+  align-self: end;
+  text-decoration: none;
+  color: var(--text-color);
+  transition: all var(--transition-duration);
+  &:hover {
+    color: var(--link-button-hover-color) !important;
+    text-decoration: underline;
+  }
+}
+.projects-landing-page__project p {
+  grid-area: description;
+}
+</style>
