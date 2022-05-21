930f2bd3acd76de3269c622ca0a92d189f4aea22
Kim LaRocca <kim@kimlarocca.com>
2022-05-21 13:35:58 -0400

    progress

--- plugins/ga.js
+++ plugins/ga.js
@@ -1,11 +1,11 @@
-import { defineNuxtPlugin } from '#app'
-import VueGtag from 'vue-gtag-next'
+// import { defineNuxtPlugin } from '#app'
+// import VueGtag from 'vue-gtag-next'
 
-// setup vue gtag as a nuxt plugin
-export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    property: {
-      id: 'G-T0Q62GL5TY'
-    }
-  })
-})
+// // setup vue gtag as a nuxt plugin
+// export default defineNuxtPlugin((nuxtApp) => {
+//   nuxtApp.vueApp.use(VueGtag, {
+//     property: {
+//       id: 'G-T0Q62GL5TY'
+//     }
+//   })
+// })
