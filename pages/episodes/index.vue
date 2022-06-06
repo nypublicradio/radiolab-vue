f63336ce0bc2afc56af74990bd9f2bf2272526bf
Thomas Bono <thomasbono.com@gmail.com>
2022-06-06 12:22:13 -0400

    clean up and docs

--- pages/episodes/index.vue
+++ pages/episodes/index.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { useRuntimeConfig } from '#app'
 import TitleWithSearch from '~/components/TitleWithSearch.vue'
-import { ref, watch } from 'vue'
+import { ref } from 'vue'
 
+/*Algolia Search START*/
 const { result, search } = useSearch('radiolab') // pass your index name as param
 const searchTerm = ref('')
 const searchYear = ref('')
@@ -13,30 +14,24 @@ const isSearching = ref(false)
 // the actual search logic to Algolia
 const searching = async () => {
   isSearching.value = true
-  // gets time stamp (ts) from beginning of the selected year
+  // gets time stamp from beginning of the selected year
   const selectedYearSeconds =
     new Date(`January 1, ${searchYear.value} 00:00:00`).getTime() / 1000
   // set length of one year in seconds
   const yearSeconds = 31556926
-  // if a search year is selected, set the search range filter to the selected year
+  // if a search year is selected, set the search range filter to the selected year timestamp TO selectedYearSeconds + yearSeconds
   const filters = searchYear.value
     ? `date-line-ts:${selectedYearSeconds} TO ${
         selectedYearSeconds + yearSeconds
       }`
     : ''
-  // console.log('searching ---------')
-  // console.log('term = ', searchTerm.value)
-  // console.log('page = ', searchPage.value)
-  // console.log('year = ', searchYear.value)
-  // console.log('filters = ', filters)
   await search({
     query: searchTerm.value,
     requestOptions: {
       page: searchPage.value,
       filters: filters,
     },
-  }).then(({ hits }) => {
-    //console.log('result   = ', result.value)
+  }).then(() => {
     searchResults.value = result.value
     setTimeout(() => {
       isSearching.value = false
@@ -44,40 +39,38 @@ const searching = async () => {
   })
 }
 
-// fired when the user presses enter or searches in the input textfield whitch triggers a Algolia search and controls the loading icon spinner state
+// fired when the user presses enter
 const onSearch = async (term) => {
   searchTerm.value = term
   searching()
 }
-// fired every time the search input is updated except with the enter key, this then detects if the field is empty, and to clear the results
+// fired every time the search input is updated except with the enter key, this then detects if the field is empty && no year is selected, and to clear the results
 const onUpdate = (event) => {
-  console.log('searchYear.value = ', searchYear.value)
   searchTerm.value = event
   if (event === '' && searchYear.value === '') {
+    // clear the results and use non-angolia episode component
     searchResults.value = null
   } else {
+    // new search: reset pagination to 0
     searchPage.value = 0
     searching()
   }
 }
 // every time the user paginates, the method is called to update the page number in the Algolia search
-const updatePaginationInfo = (event) => {
+const onUpdatePaginationInfo = (event) => {
   searchPage.value = event.page
   searching()
 }
 // every time the user selects a year filter, the method is called to update the filter in the Algolia search
 const onYearFilter = (yearValue) => {
-  /* console.log('onYearFilter -------------')
-  console.log('yearValue = ', yearValue) */
-  const thisTerm = searchTerm.value ? searchTerm.value : ''
   const thisYear = isNaN(yearValue) ? '' : yearValue
-  /*  console.log('thisTerm = ', thisTerm)
-  console.log('thisYear = ', thisYear) */
-  searchTerm.value = thisTerm
+
   searchYear.value = thisYear
+  // new search: reset pagination to 0
   searchPage.value = 0
   searching()
 }
+/*Algolia Search END*/
 
 const config = useRuntimeConfig()
 const apiUrl = `${config.API_URL}/api/v3/channel/shows/radiolab/recent_stories/`
@@ -122,7 +115,7 @@ const apiUrl = `${config.API_URL}/api/v3/channel/shows/radiolab/recent_stories/`
       :rowsPerAd="2"
       :episodes-search-results="searchResults"
       :paginate="true"
-      @search-page-event="updatePaginationInfo"
+      @search-page-event="onUpdatePaginationInfo"
     />
     <episodes
       v-else
