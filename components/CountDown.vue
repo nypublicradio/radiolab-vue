32ab122b5d2abb09e982fc72b385d5b96807adf5
Kelly Suitor <kelly.suitor@gmail.com>
2024-09-06 10:45:05 -0400

    Bono Updates incl Countdown and Banner

---
+++ components/CountDown.vue
@@ -0,0 +1,74 @@
+<script setup>
+import { ref, computed, onMounted, onUnmounted } from "vue"
+
+const props = defineProps({
+  targetDate: {
+    type: Date,
+    required: true,
+  },
+})
+
+const now = ref(new Date())
+
+const remainingTime = computed(() => {
+  const diff = props.targetDate.getTime() - now.value.getTime()
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24))
+  const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+  const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60))
+  const seconds = Math.floor((diff % (1000 * 60)) / 1000)
+
+  return { days, hours, minutes, seconds }
+})
+function padZero(num) {
+  return num.toString().padStart(2, "0")
+}
+let intervalId
+
+onMounted(() => {
+  intervalId = setInterval(() => {
+    now.value = new Date()
+  }, 1000)
+})
+
+onUnmounted(() => {
+  clearInterval(intervalId)
+})
+</script>
+
+<template>
+  <div class="countdown">
+    <div class="time-display flex gap-3 md:gap-5">
+      <div class="time-unit">{{ padZero(remainingTime.days) }} <span>days</span></div>
+      <div class="time-unit">{{ padZero(remainingTime.hours) }} <span>hours</span></div>
+      <div class="time-unit">
+        {{ padZero(remainingTime.minutes) }} <span>minutes</span>
+      </div>
+      <div class="time-unit">
+        {{ padZero(remainingTime.seconds) }} <span>seconds</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.countdown {
+  text-align: center;
+  margin-top: 1.5rem;
+}
+
+.time-display {
+  font-size: 3rem;
+  @include media("<md") {
+    font-size: 1.25rem;
+  }
+  .time-unit {
+    text-shadow: #858cb1 0px 0px 30px;
+    span {
+      text-shadow: #858cb1 0px 0px 20px;
+      font-size: 1rem;
+      display: block;
+      color: #858cb1;
+    }
+  }
+}
+</style>
--- pages/quasi-moon/index.vue
+++ pages/quasi-moon/index.vue
@@ -1,27 +1,27 @@
 <script setup>
-import { onBeforeMount, ref } from 'vue'
-import axios from 'axios'
-import VCard from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue'
-import { formatPublisherImageUrl } from '~/utilities/helpers'
-import { useRuntimeConfig } from '#app'
-import teamData from './team-data'
+import { onBeforeMount, ref } from "vue"
+import axios from "axios"
+import VCard from "@nypublicradio/nypr-design-system-vue3/v2/src/components/VCard.vue"
+import { formatPublisherImageUrl } from "~/utilities/helpers"
+import { useRuntimeConfig } from "#app"
+import teamData from "./team-data"
 
-import VFlexibleLink from '@nypublicradio/nypr-design-system-vue3/v2/src/components/VFlexibleLink.vue'
-import colors from '~/assets/scss/colors.module.scss'
+import VFlexibleLink from "@nypublicradio/nypr-design-system-vue3/v2/src/components/VFlexibleLink.vue"
+import colors from "~/assets/scss/colors.module.scss"
 useHead({
   meta: [
     {
-      name: 'theme-color',
+      name: "theme-color",
       content: colors.rlBlue,
     },
   ],
   bodyAttrs: {
-    class: 'has-head-color quasi-moon invert-menu-color',
+    class: "has-head-color quasi-moon invert-menu-color",
   },
 })
 const { $analytics } = useNuxtApp()
 
-console.log('teamData', teamData)
+console.log("teamData", teamData)
 const config = useRuntimeConfig()
 const dataLoaded = ref(false)
 const people = ref([])
@@ -38,13 +38,15 @@ onMounted(async () => {
   //     totalCount.value = response.data.data.attributes['total-count']
   //     dataLoaded.value = true
   //   })
-  people.value = teamData.included;
-  totalCount.value = teamData.data.attributes['total-count'];
-  dataLoaded.value = true;
+  people.value = teamData.included
+  totalCount.value = teamData.data.attributes["total-count"]
+  dataLoaded.value = true
 })
 
 const goToPersonPage = (slug) => {
-  if(slug){window.open(slug, "_blank")}
+  if (slug) {
+    window.open(slug, "_blank")
+  }
 }
 </script>
 
@@ -53,19 +55,43 @@ const goToPersonPage = (slug) => {
     <Html lang="en">
       <Head>
         <Title>Name a Quasi-moon Contest | Radiolab | WNYC Studios</Title>
-        <Meta name="og:title" content="Name a Quasi-moon Contest | Radiolab | WNYC Studios" />
+        <Meta
+          name="og:title"
+          content="Name a Quasi-moon Contest | Radiolab | WNYC Studios"
+        />
         <Meta
           name="twitter:title"
           content="Name a Quasi-moon Contest | Radiolab | WNYC Studios"
         />
       </Head>
     </Html>
-    <!-- <section class="quasi-moon graphic-head flex justify-content-center align-content-center"> -->
-    <section class="quasi-moon">
+    <section
+      class="quasi-moon graphic-head-bg flex justify-content-center align-content-center"
+    >
+      <div
+        class="flex flex-column justify-content-center align-content-center pb-8 shape-holder"
+      >
+        <img src="/shape_result.webp" class="shape" />
+        <h1 class="title h2 gold text-center lg:text-7xl">Name a Quasi-Moon</h1>
+      </div>
+    </section>
+    <section class="below-head -mt-8">
+      <div class="content py-0">
+        <h4 class="gold md:px-8">
+          An oppertunity to rays ephemeris zodiac cosmology red shift azimuth perigee
+          inertia exoplanet radiant gravity deep space white dwarf
+        </h4>
+      </div>
+    </section>
+    <section class="count-down px-2 py-6 lg:p-8">
+      <h2 class="text-center text-white font-normal">Voting begins in...</h2>
+      <CountDown :targetDate="new Date('2024-11-01T00:00:00')" />
+    </section>
+    <section>
       <div class="content xl:px-8 pb-0">
         <div class="grid">
           <div class="col-12">
-            <h1 class="h2 font-normal quasi-moon">Name A Quasi-Moon</h1>
+            <h1 class="h2 font-normal text-white">Meet the Panel</h1>
           </div>
         </div>
       </div>
@@ -77,25 +103,19 @@ const goToPersonPage = (slug) => {
             v-for="(person, index) in people"
             :key="`${person.attributes.person.name}-${index}`"
             class="col-12 sm:col-6 lg:col-4 mb-6"
-            :class="[{'cursor-pointer': person.attributes.person.slug}]"
+            :class="[{ 'cursor-pointer': person.attributes.person.slug }]"
             @click="goToPersonPage(person.attributes.person.slug)"
           >
             <client-only>
               <v-card
-                :image="
-                  formatPublisherImageUrl(
-                    person.attributes.person.image.template
-                  )
-                "
+                :image="formatPublisherImageUrl(person.attributes.person.image.template)"
                 :width="320"
                 :height="240"
                 :alt="person.attributes.person.name"
                 :title="person.attributes.person.name"
                 :titleLink="`/quasi-moon/${person.attributes.person.slug}`"
                 :eyebrow="person.attributes['appearance-type']"
-                :blurb="
-                  person.attributes.person.lede || person.attributes.person.bio
-                "
+                :blurb="person.attributes.person.lede || person.attributes.person.bio"
                 :max-width="person.attributes.person.image.w"
                 :max-height="person.attributes.person.image.h"
                 :ratio="[3, 2]"
@@ -121,52 +141,82 @@ const goToPersonPage = (slug) => {
 </template>
 
 <style lang="scss">
-
 .has-head-color.quasi-moon {
   background-color: #000000;
-  color: #FFFFFF;
+  color: #ffffff;
   padding: 2rem 0;
-  // .content {
-  //   max-width: 1200px;
-  //   margin: 0 auto;
-  // }
 }
-
-.graphic-head {
+.quasi-moon {
+  .graphic-head-bg {
     background-color: #000000;
-    background-image: url('/rl-icon-on-blue2.svg');
+    background-image: url("/stars_result.webp");
+    background-position: center bottom;
+    background-size: cover;
+    background-size: cover;
+    .shape-holder {
+      width: 100%;
+      background-color: #00000080;
+    }
+    .title {
+      position: absolute;
+      left: 0;
+      right: 0;
+      margin: auto;
+    }
+    .shape {
+      width: 100%;
+      max-width: 430px;
+      margin: 0 auto;
+      opacity: 0.5;
+    }
   }
 
-.h2.quasi-moon {
-  color: #FFFFFF;
-}
-
-.team .cards > .grid {
-  margin: 0 -24px;
-}
-
-.team .cards .grid > .col,
-.team .cards .grid > [class*='col'] {
-  padding: 0 24px;
-}
-
-.team .cards .card-eyebrow {
-  text-transform: capitalize;
-}
-
-.team .card-details {
-  justify-content: flex-start !important;
-  .card-blurb {
-    -webkit-line-clamp: unset !important;
+  .below-head {
+    h4 {
+      font-weight: var(--font-weight-400);
+      font-size: var(--font-size-10);
+      line-height: var(--font-size-13);
+      text-align: center;
+      letter-spacing: 10px;
+      @include media("<md") {
+        font-size: var(--font-size-6);
+        line-height: var(--font-size-9);
+        letter-spacing: 6px;
+      }
+    }
   }
-}
 
-.radiolab-card.quasi-moon {
-  pointer-events: none;
-  .card-details {
+  .gold {
+    color: #fff322;
+  }
+
+  .team .cards > .grid {
+    margin: 0 -24px;
+  }
+
+  .team .cards .grid > .col,
+  .team .cards .grid > [class*="col"] {
+    padding: 0 24px;
+  }
+
+  .team .cards .card-eyebrow {
+    text-transform: capitalize;
+  }
+
+  .team .card-details {
     justify-content: flex-start !important;
     .card-blurb {
       -webkit-line-clamp: unset !important;
+    }
+  }
+
+  .radiolab-card.quasi-moon {
+    pointer-events: none;
+    .card-details {
+      justify-content: flex-start !important;
+      .card-blurb {
+        -webkit-line-clamp: unset !important;
+      }
     }
   }
 }
